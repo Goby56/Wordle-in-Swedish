f3f9572feb869a47abc60b1859fbd3d168f849c3
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-02-12 11:49:16 +0100

    Word list

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
 let guessList = [];
 
 function submitGuess() {
-    guessList.push(document.getElementById("guessInput").value);
-}
-
-async function filterWords() {
-    let data = await fetchFile("words.txt");
-    data = data.split("\n");
-    let number;
-    for (i = 0; i < data.length; i++) {
-        if (data[i] != 5) {
-            number++;
-        }
-    }
-    console.log(number);
-}
-
-async function fetchFile(filename) {
-    const response = await fetch(filename);
-    return response.text();
+    let guess = document.getElementById("guessInput").value;
+    guessList.push(guess);
+    document.getElementById("guessesDisplay").innerHTML = guessList;
 }
