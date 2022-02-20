ab626c3dd0d9103ed848285808419e4be674f034
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-02-20 21:53:15 +0100

    Color checking

--- script.js
+++ script.js
@@ -1,49 +1,95 @@
-let guesses = 0;
+async function getValidWords() {
+    const response = await fetch("./words.txt");
+    const validWords = (await response.text()).split("\n");
+    for (let i = 0; i < validWords.length; i++) {
+        validWords[i] = validWords[i].replace("\r", "");
+    }
+    return validWords;
+}
+
+// Run as soon as the page loads in
+document.addEventListener("DOMContentLoaded", reset());
+
+let randomWordID = randomNumberGenerator(0, 8190); // Hard coded number of available words
+let guesses = [];
+let numberOfGuesses = 0;
 let position = 0;
 let tileElements = document.getElementsByClassName("tile");
+let gameRunning = true;
 
-document.addEventListener("keydown", (event) => {
+bgColor = "#121213";
+borderColor = "#818384";
+absentColor = "#3a3a3c";
+textColor = "#ffffff";
+presentColor = "#b59f3b";
+correctColor = "#538d4e";
+
+async function reset() {
+    guesses = [];
+    numberOfGuesses = 0;
+    position = 0;
+
+    for (let tile of tileElements) {
+        tile.innerText = "";
+        tile.style.backgroundColor = bgColor;
+    }
+}
+
+async function showCorrectWord() {
+    var validWords = await getValidWords();
+    var correctWord = validWords[randomWordID];
+    document.getElementById("display-correct-word").innerHTML = correctWord;
+}
+
+document.addEventListener("keydown", async (event) => {
     position %= 6;
     if ("abcdefghijklmnopqrstuvwxyzåäö".includes(event.key)) {
         if (position < 5) {
             addChar(event.key);
-            position++;
         }
     } else if (event.code === "Backspace") {
         if (position > 0) {
-            position--;
             delChar();
         }
     } else if (event.code == "Enter") {
-        if (guesses < 5 && position === 5) {
-            let word = "";
+        if (numberOfGuesses < 6 && position === 5) {
+            let guess = "";
             for (let i = 0; i < 5; i++) {
-                word += tileElements[guesses * 5 + i].innerHTML;
+                guess += tileElements[numberOfGuesses * 5 + i].innerHTML;
             }
-            submitWord(word);
+            let validWords = await getValidWords();
+            if (validWords.includes(guess)) {
+                submitGuess(guess, validWords[randomWordID]);
+            }
         }
     }
-    /*console.log(`Guess : ${guesses}, Posistion : ${position}`);*/
 });
 
 function addChar(key) {
-    tileElements[guesses * 5 + position].innerText = key;
+    tileElements[numberOfGuesses * 5 + position].innerText = key;
+    position++;
 }
 
 function delChar() {
-    tileElements[guesses * 5 + position].innerText = "";
+    position--;
+    tileElements[numberOfGuesses * 5 + position].innerText = "";
 }
 
-function submitWord(word) {
-    fetch("./words.txt")
-        .then((response) => response.text())
-        .then(function (textTile) {
-            if (textTile.includes(word)) {
-                alert("Word in list");
-                guesses++;
-                position = 0;
-            } else {
-                alert("Word not found");
-            }
-        });
+async function submitGuess(guess, correctWord) {
+    for (let i = 0; i < 5; i++) {
+        if (guess[i] === correctWord[i]) {
+            tileElements[numberOfGuesses * 5 + i].style.backgroundColor = correctColor;
+        } else if (correctWord.includes(guess[i])) {
+            tileElements[numberOfGuesses * 5 + i].style.backgroundColor = presentColor;
+        } else {
+            tileElements[numberOfGuesses * 5 + i].style.backgroundColor = absentColor;
+        }
+    }
+    numberOfGuesses++;
+    position = 0;
+    guesses.push(guess);
+}
+
+function randomNumberGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
