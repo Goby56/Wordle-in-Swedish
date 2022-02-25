53301d6c428a429afe325ffb199fab4cfdbd3e04
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-02-25 23:59:17 +0100

    Improved word list

--- swedish wordle parser.py
+++ swedish wordle parser.py
@@ -1,6 +1,22 @@
-with open("words.txt", "r") as f:
+# -*- coding: UTF-8 -*-
+
+def is_accepted(word):
+    for char in word:
+        if char not in "abcdefghijklmnopqrstuvwxyzåäö":
+            return False
+    return True
+
+with open("svenska-ord.txt", "r", encoding="utf-8") as f:
     words = f.read().split("\n")
-    for i in range(len(words)):
-        if words[i] != 5:
-            words.pop(i)
-    print(words)
+
+filtered_words = []
+for word in words:
+    word = word.strip().lower()
+    if " " in word:
+        continue
+    if len(word) == 5:
+        if is_accepted(word):
+            filtered_words.append(word+"\n")
+
+with open("valid-words.txt", "w", encoding="utf-8") as f:
+    f.writelines(filtered_words)
